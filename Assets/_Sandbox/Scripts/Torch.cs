046faada834c312ffe3bf98ee90a5630709cea22
Aleksi A <djoah003@edu.xamk.fi>
2024-03-11 10:06:24 +0200

    I don't remember what I did

--- Assets/_Sandbox/Scripts/Torch.cs
+++ Assets/_Sandbox/Scripts/Torch.cs
@@ -9,43 +9,31 @@ public class Torch : MonoBehaviour
 {
     [SerializeField] private Light _light;
     public GameObject avatar;
-
-    [Range(0, 360)]
-    public float rotationValueTest;
+    
     // Start is called before the first frame update
-
     private void OnDrawGizmos()
     { 
         float range = _light.range;
-        float angle = _light.spotAngle;
-        Vector3 start = _light.transform.position;
-        Vector3 f = _light.transform.forward;
+        // spotAngle uses the lights OuterSpotAngle value. Divide it by 2 to be able to use it creating triangles.
+        float angle = _light.spotAngle / 2;
+        // The beginning of the triangle
+        Vector3 start = avatar.transform.position;
         
-        Vector3 forwardVector = (start + f);
+        Vector3 forwardVector = (start + avatar.transform.forward);
         Vector3.Normalize(forwardVector);
         forwardVector.z *= range;
-       
-        Vector3 vertexB = start + Quaternion.Euler(0, angle / 2, 0f) * Vector3.forward * range;
-        Vector3 vertexC = start + Quaternion.Euler(0, -angle / 2, 0f) * Vector3.forward * range;
 
+        Vector3 leftTri = Quaternion.Euler(0, -angle, 0f) * forwardVector;
+        Vector3 rightTri = Quaternion.Euler(0, angle, 0f) * forwardVector;
+        
         Gizmos.color = Color.red;
         Gizmos.DrawLine(start, forwardVector);
         Gizmos.color = Color.green;
-        Gizmos.DrawLine(start, vertexB);
-        Gizmos.DrawLine(start, vertexC);
-        Gizmos.DrawLine(vertexC, vertexB);
-        
+        Gizmos.DrawLine(start, leftTri);
+        Gizmos.DrawLine(start, rightTri);
+        Gizmos.DrawLine(leftTri, rightTri);
     }
     
-    Vector3 RotateVector(Vector3 originalVector, Vector3 rotationAxis, float rotationValue)
-    {
-        // Create a rotation quaternion
-        Quaternion rotationQuaternion = Quaternion.AngleAxis(rotationValue, rotationAxis);
-        // Rotate the vector
-        Vector3 rotatedVector = rotationQuaternion * originalVector;
-
-        return rotatedVector;
-    }
 
     void Start()
     {
